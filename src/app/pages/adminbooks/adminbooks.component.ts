082b4d0ab7a7b3f7fef8240630c3d6cdd61226fc
Jonnathan23 <jsaquicela26@gmail.com>
2024-07-27 01:36:52 -0500

    Cominicacion con la ventana emergente del java xhtml

--- src/app/pages/adminbooks/adminbooks.component.ts
+++ src/app/pages/adminbooks/adminbooks.component.ts
@@ -12,14 +12,20 @@ import { LoginserviceService } from '../../services/foruser/loginservice.service
   styleUrl: './adminbooks.component.scss'
 })
 export class AdminbooksComponent {
-  user! :UserType
+  user!: UserType
 
-  constructor(private loginService:LoginserviceService){}
+  constructor(private loginService: LoginserviceService) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.loginService.getUserActive().subscribe((user) => {
-      const userLocal = this.loginService.getUserStorage()!; 
-       this.user = userLocal.us_id ? userLocal : user;
+      const userLocal = this.loginService.getUserStorage()!;
+      this.user = userLocal.us_id ? userLocal : user;
+
+      // !Eliminar está linea
+      this.user.us_id = 2
+
+      window.open(`http://localhost:8080/proyectobackend/faces/notification.xhtml?userId=${this.user.us_id}`, '_blank');
+
     })
   }
 }
