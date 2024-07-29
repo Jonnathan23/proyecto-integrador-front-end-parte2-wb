d535a24a7ab6034ef7fb99b706a3ef38e14303b1
Erika Villa <114946749+Erika05cristina@users.noreply.github.com>
2024-07-29 11:35:59 -0500

    Busqueda avanzada

--- src/app/pages/adminbooks/admin/admin.component.ts
+++ src/app/pages/adminbooks/admin/admin.component.ts
@@ -5,6 +5,8 @@ import { AddbookComponent } from "./addbook/addbook.component";
 import { SelectedbookService } from '../../../services/forbook/selectedbook.service';
 import { DatabookService } from '../../../services/forbook/databook.service';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
+import { DefaultErrorAngular } from '../../../../errors/errors';
 
 @Component({
   selector: 'app-admin',
@@ -14,6 +16,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+  private destroy$ = new Subject<void>();
   books: AdminBook[] = [];
 
   adminLibrary = true
@@ -24,17 +27,71 @@ export class AdminComponent {
     this.getBooks()
   }
 
+  /** @description cancela la suscripcion */
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  /**
+   * @description funcion del buscador
+   * @param event 
+   */
+  onSearch(data: string) {
+    if (data) {
+      this.bookService.searchNameBooks(data).pipe(takeUntil(this.destroy$)).subscribe({
+        next: (booksName) => booksName.length ? this.books = booksName : this.searchCategory(data)
+        , error: error => DefaultErrorAngular.emitAlert()
+      })
+    } else {
+      this.getBooks()
+    }
+
+  }
+
+  /**
+   * @description busca los libros por categoria
+   * @param data 
+   */
+  searchCategory(data: string) {
+    this.bookService.searchCategoryBooks(data).pipe(takeUntil(this.destroy$)).subscribe({
+      next: booksCategory => booksCategory.length ? this.books = booksCategory : this.searchAutor(data)
+      , error: () => DefaultErrorAngular.emitAlert()
+    })
+  }
+
+  /**
+   * @description busca los libros por el nombre del autor
+   * @param data 
+   */
+  searchAutor(data: string) {
+    this.bookService.searchAutorBooks(data).pipe(takeUntil(this.destroy$)).subscribe({
+      next: booksAutor => booksAutor.length ? this.books = booksAutor : this.searchAvailability(data)
+      , error: () => DefaultErrorAngular.emitAlert()
+    })
+  }
+
+  /**
+   * @description busca los libros por el nombre
+   * @param data 
+   */
+  searchAvailability(data: string) {
+    this.bookService.searchAvailabilityBooks(data).pipe(takeUntil(this.destroy$)).subscribe({
+      next: booksAvailability => this.books = booksAvailability.length ? booksAvailability : []
+      , error: () => DefaultErrorAngular.emitAlert()
+    })
+  }
+
   getBooks() {
-    this.bookService.getBooks().subscribe(
-      data => {
+    this.bookService.getBooks().pipe(takeUntil(this.destroy$)).subscribe({
+      next: data => {
         this.books = data
         this.cd.detectChanges()
-      },
-      error => {
+      }, error: () => {
         this.books = []
-        console.error(error)
+        DefaultErrorAngular.emitAlert()
       }
-    )
+    })
   }
 
   /**
